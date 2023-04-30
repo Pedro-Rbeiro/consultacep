d6eb4d1cd89a0f504a7321757b0286bcff22cf9a
pedro-rbeiro <business.psycho@outlook.com>
2023-04-30 13:09:45 -0300

    Copy Function

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const bairro = document.getElementById("bairro");
 const cidade = document.getElementById("cidade");
 const estado = document.getElementById("estado");
 const resultados = document.getElementById("results");
+cep.value = "01001000";
 resultados.style = "display:none";
 cep.addEventListener("blur", function (e) {
   let cep = e.target.value;
@@ -18,22 +19,25 @@ function popularform(resposta) {
     alert("cep não encontrado!");
     return;
   }
-
-  console.log(resposta);
+  // console.log(resposta);
   rua.value = resposta.logradouro;
   bairro.value = resposta.bairro;
   cidade.value = resposta.localidade;
   estado.value = resposta.uf;
   if (rua !== null) {
     resultados.style = "display:visible";
-    resultados.innerText =
-      "Resultados para o CEP informado: " +
-      rua.value +
-      ", " +
-      bairro.value +
-      ", " +
-      cidade.value +
-      ", " +
-      estado.value;
+    resultados.innerHTML = `Resultados para o CEP informado: <span id="results-text">${rua.value} ${bairro.value} ${cidade.value} ${estado.value}<span>`;
+    // resultados.id = "results-text";
   }
+  const resultadoText = document.getElementById("results-text");
+  resultadoText.addEventListener("click", async function (e) {
+    try {
+      await navigator.clipboard.writeText(resultadoText.innerText);
+      const infoResults = document.getElementById("info-results");
+      infoResults.style = "display:visible";
+      infoResults.innerText = `Texto copiado com sucesso!`;
+    } catch (err) {
+      console.log("Failed to copy text");
+    }
+  });
 }
